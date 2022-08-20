033f9f0dc4fa4105d8da3e22814c067c6a1cac24
pcHwang2 <15112336606@163.com>
2022-08-20 11:51:17 +0800

    support OffsetFetchRespV7 encode

--- codec/offset_fetch_resp.go
+++ codec/offset_fetch_resp.go
@@ -56,45 +56,45 @@ type OffsetFetchPartitionResp struct {
 
 func (o *OffsetFetchResp) BytesLength(version int16) int {
 	result := LenCorrId
-	if version == 6 {
+	if version == 6 || version == 7 {
 		result += LenTaggedField + LenThrottleTime
 	}
 	if version == 1 {
 		result += LenArray
-	} else if version == 6 {
+	} else if version == 6 || version == 7 {
 		result += CompactArrayLen(len(o.TopicRespList))
 	}
 	for _, val := range o.TopicRespList {
 		if version == 1 {
 			result += StrLen(val.Topic)
-		} else if version == 6 {
+		} else if version == 6 || version == 7 {
 			result += CompactStrLen(val.Topic)
 		}
 		if version == 1 {
 			result += LenArray
-		} else if version == 6 {
+		} else if version == 6 || version == 7 {
 			result += CompactArrayLen(len(val.PartitionRespList))
 		}
 		for _, val2 := range val.PartitionRespList {
 			result += LenPartitionId + LenOffset
-			if version == 6 {
+			if version == 6 || version == 7 {
 				result += LenLeaderEpoch
 			}
 			if version == 1 {
 				result += StrLen(*val2.Metadata)
-			} else if version == 6 {
+			} else if version == 6 || version == 7 {
 				result += CompactNullableStrLen(val2.Metadata)
 			}
 			result += LenErrorCode
-			if version == 6 {
+			if version == 6 || version == 7 {
 				result += LenTaggedField
 			}
 		}
-		if version == 6 {
+		if version == 6 || version == 7 {
 			result += LenTaggedField
 		}
 	}
-	if version == 6 {
+	if version == 6 || version == 7 {
 		result += LenErrorCode + LenTaggedField
 	}
 	return result
@@ -104,47 +104,47 @@ func (o *OffsetFetchResp) Bytes(version int16) []byte {
 	bytes := make([]byte, o.BytesLength(version))
 	idx := 0
 	idx = putCorrId(bytes, idx, o.CorrelationId)
-	if version == 6 {
+	if version == 6 || version == 7 {
 		idx = putTaggedField(bytes, idx)
 		idx = putThrottleTime(bytes, idx, o.ThrottleTime)
 	}
 	if version == 1 {
 		idx = putArrayLen(bytes, idx, len(o.TopicRespList))
-	} else if version == 6 {
+	} else if version == 6 || version == 7 {
 		idx = putCompactArrayLen(bytes, idx, len(o.TopicRespList))
 	}
 	for _, topic := range o.TopicRespList {
 		if version == 1 {
 			idx = putTopicString(bytes, idx, topic.Topic)
-		} else if version == 6 {
+		} else if version == 6 || version == 7 {
 			idx = putTopic(bytes, idx, topic.Topic)
 		}
 		if version == 1 {
 			idx = putArrayLen(bytes, idx, len(topic.PartitionRespList))
-		} else if version == 6 {
+		} else if version == 6 || version == 7 {
 			idx = putCompactArrayLen(bytes, idx, len(topic.PartitionRespList))
 		}
 		for _, partition := range topic.PartitionRespList {
 			idx = putPartitionId(bytes, idx, partition.PartitionId)
 			idx = putOffset(bytes, idx, partition.Offset)
-			if version == 6 {
+			if version == 6 || version == 7 {
 				idx = putLeaderEpoch(bytes, idx, partition.LeaderEpoch)
 			}
 			if version == 1 {
 				idx = putString(bytes, idx, *partition.Metadata)
-			} else if version == 6 {
+			} else if version == 6 || version == 7 {
 				idx = putMetadata(bytes, idx, partition.Metadata)
 			}
 			idx = putErrorCode(bytes, idx, partition.ErrorCode)
-			if version == 6 {
+			if version == 6 || version == 7 {
 				idx = putTaggedField(bytes, idx)
 			}
 		}
-		if version == 6 {
+		if version == 6 || version == 7 {
 			idx = putTaggedField(bytes, idx)
 		}
 	}
-	if version == 6 {
+	if version == 6 || version == 7 {
 		idx = putErrorCode(bytes, idx, o.ErrorCode)
 		idx = putTaggedField(bytes, idx)
 	}
