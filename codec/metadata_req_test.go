205eb4fcfb80ec636a1e7c9287f62814244f69f6
pcHwang2 <15112336606@163.com>
2022-08-30 15:30:57 +0800

    support MetadataV0-V9 decode and encode

--- codec/metadata_req_test.go
+++ codec/metadata_req_test.go
@@ -42,6 +42,37 @@ func TestDecodeMetadataV1(t *testing.T) {
 	assert.False(t, metadataTopicReq.IncludeTopicAuthorizedOperations)
 }
 
+func TestCodeMetadataV1(t *testing.T) {
+	metadataReq := &MetadataReq{}
+	metadataReq.ApiVersion = 1
+	metadataReq.CorrelationId = 1
+	metadataReq.ClientId = "___TestKafkaProduce_in_go_demo_demo_kafka.test@hezhangjiandeMacBook-Pro.local (github.com/segmentio/kafka-go)"
+	metadataReq.Topics = []*MetadataTopicReq{
+		{
+			Topic: "topic",
+		},
+	}
+	bytes := metadataReq.Bytes(true)
+	expectBytes := testHex2Bytes(t, "0003000100000001006d5f5f5f546573744b61666b6150726f647563655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29000000010005746f706963")
+	assert.Equal(t, expectBytes, bytes)
+}
+
+func TestDecodeAndEncodeMetadataV1(t *testing.T) {
+	bytes := testHex2Bytes(t, "00000001006d5f5f5f546573744b61666b6150726f647563655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29000000010005746f706963")
+	metadataTopicReq, err := DecodeMetadataReq(bytes, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, metadataTopicReq.CorrelationId)
+	assert.Equal(t, "___TestKafkaProduce_in_go_demo_demo_kafka.test@hezhangjiandeMacBook-Pro.local (github.com/segmentio/kafka-go)", metadataTopicReq.ClientId)
+	assert.Len(t, metadataTopicReq.Topics, 1)
+	topicReq := metadataTopicReq.Topics[0]
+	assert.Equal(t, "topic", topicReq.Topic)
+	assert.False(t, metadataTopicReq.AllowAutoTopicCreation)
+	assert.False(t, metadataTopicReq.IncludeClusterAuthorizedOperations)
+	assert.False(t, metadataTopicReq.IncludeTopicAuthorizedOperations)
+	codeBytes := metadataTopicReq.Bytes(false)
+	assert.Equal(t, bytes, codeBytes)
+}
+
 func TestDecodeMetadataV8(t *testing.T) {
 	bytes := testHex2Bytes(t, "00000001000a70726f64756365722d3100000001000a746573742d746f706963010000")
 	metadataTopicReq, err := DecodeMetadataReq(bytes, 8)
@@ -51,9 +82,43 @@ func TestDecodeMetadataV8(t *testing.T) {
 	assert.Len(t, metadataTopicReq.Topics, 1)
 	topicReq := metadataTopicReq.Topics[0]
 	assert.Equal(t, "test-topic", topicReq.Topic)
-	assert.False(t, metadataTopicReq.AllowAutoTopicCreation)
+	assert.True(t, metadataTopicReq.AllowAutoTopicCreation)
 	assert.False(t, metadataTopicReq.IncludeClusterAuthorizedOperations)
 	assert.False(t, metadataTopicReq.IncludeTopicAuthorizedOperations)
+}
+
+func TestEncodeMetadataReqV8(t *testing.T) {
+	metadataReq := &MetadataReq{}
+	metadataReq.ApiVersion = 8
+	metadataReq.CorrelationId = 1
+	metadataReq.ClientId = "producer-1"
+	metadataReq.Topics = []*MetadataTopicReq{
+		{
+			Topic: "test-topic",
+		},
+	}
+	metadataReq.AllowAutoTopicCreation = true
+	metadataReq.IncludeClusterAuthorizedOperations = false
+	metadataReq.IncludeTopicAuthorizedOperations = false
+	bytes := metadataReq.Bytes(true)
+	expectBytes := testHex2Bytes(t, "0003000800000001000a70726f64756365722d3100000001000a746573742d746f706963010000")
+	assert.Equal(t, expectBytes, bytes)
+}
+
+func TestDecodeAndEncodeMetadataReqV8(t *testing.T) {
+	bytes := testHex2Bytes(t, "00000001000a70726f64756365722d3100000001000a746573742d746f706963010000")
+	metadataTopicReq, err := DecodeMetadataReq(bytes, 8)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, metadataTopicReq.CorrelationId)
+	assert.Equal(t, "producer-1", metadataTopicReq.ClientId)
+	assert.Len(t, metadataTopicReq.Topics, 1)
+	topicReq := metadataTopicReq.Topics[0]
+	assert.Equal(t, "test-topic", topicReq.Topic)
+	assert.True(t, metadataTopicReq.AllowAutoTopicCreation)
+	assert.False(t, metadataTopicReq.IncludeClusterAuthorizedOperations)
+	assert.False(t, metadataTopicReq.IncludeTopicAuthorizedOperations)
+	codeBytes := metadataTopicReq.Bytes(false)
+	assert.Equal(t, bytes, codeBytes)
 }
 
 func TestDecodeMetadataV9(t *testing.T) {
@@ -65,7 +130,7 @@ func TestDecodeMetadataV9(t *testing.T) {
 	assert.Len(t, metadataTopicReq.Topics, 1)
 	topicReq := metadataTopicReq.Topics[0]
 	assert.Equal(t, "764edee3-007e-48e0-b9f9-df7f713ff707", topicReq.Topic)
-	assert.False(t, metadataTopicReq.AllowAutoTopicCreation)
+	assert.True(t, metadataTopicReq.AllowAutoTopicCreation)
 	assert.False(t, metadataTopicReq.IncludeClusterAuthorizedOperations)
 	assert.False(t, metadataTopicReq.IncludeTopicAuthorizedOperations)
 }
@@ -83,7 +148,23 @@ func TestEncodeMetadataReqV9(t *testing.T) {
 	metadataReq.AllowAutoTopicCreation = true
 	metadataReq.IncludeClusterAuthorizedOperations = false
 	metadataReq.IncludeTopicAuthorizedOperations = false
-	bytes := metadataReq.Bytes()
+	bytes := metadataReq.Bytes(true)
 	expectBytes := testHex2Bytes(t, "0003000900000002002f636f6e73756d65722d61316531323336352d646466612d343366632d383236652d3936363166623534633237342d31000207746573742d330001000000")
 	assert.Equal(t, expectBytes, bytes)
+}
+
+func TestDecodeAndEncodeMetadataReqV9(t *testing.T) {
+	bytes := testHex2Bytes(t, "00000002002f636f6e73756d65722d37336664633964612d306439322d346537622d613761372d6563323636663637633137312d3100022537363465646565332d303037652d343865302d623966392d6466376637313366663730370001000000")
+	metadataTopicReq, err := DecodeMetadataReq(bytes, 9)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, metadataTopicReq.CorrelationId)
+	assert.Equal(t, "consumer-73fdc9da-0d92-4e7b-a7a7-ec266f67c171-1", metadataTopicReq.ClientId)
+	assert.Len(t, metadataTopicReq.Topics, 1)
+	topicReq := metadataTopicReq.Topics[0]
+	assert.Equal(t, "764edee3-007e-48e0-b9f9-df7f713ff707", topicReq.Topic)
+	assert.True(t, metadataTopicReq.AllowAutoTopicCreation)
+	assert.False(t, metadataTopicReq.IncludeClusterAuthorizedOperations)
+	assert.False(t, metadataTopicReq.IncludeTopicAuthorizedOperations)
+	codeBytes := metadataTopicReq.Bytes(false)
+	assert.Equal(t, bytes, codeBytes)
 }
--- codec/metadata_resp.go
+++ codec/metadata_resp.go
@@ -72,9 +72,11 @@ func DecodeMetadataResp(bytes []byte, version int16) (metadataResp *MetadataResp
 	if version == 9 {
 		idx = readTaggedField(bytes, idx)
 	}
-	metadataResp.ThrottleTime, idx = readThrottleTime(bytes, idx)
+	if version > 2 && version <= 9 {
+		metadataResp.ThrottleTime, idx = readThrottleTime(bytes, idx)
+	}
 	var length int
-	if version == 1 {
+	if version < 9 {
 		length, idx = readArrayLen(bytes, idx)
 	} else if version == 9 {
 		length, idx = readCompactArrayLen(bytes, idx)
@@ -83,24 +85,32 @@ func DecodeMetadataResp(bytes []byte, version int16) (metadataResp *MetadataResp
 	for i := 0; i < length; i++ {
 		brokerMetadata := &BrokerMetadata{}
 		brokerMetadata.NodeId, idx = readNodeId(bytes, idx)
-		if version == 1 {
+		if version < 9 {
 			brokerMetadata.Host, idx = readString(bytes, idx)
 		} else if version == 9 {
 			brokerMetadata.Host, idx = readCompactString(bytes, idx)
 		}
 		brokerMetadata.Port, idx = readBrokerPort(bytes, idx)
-		brokerMetadata.Rack, idx = readRack(bytes, idx)
+		if version > 0 && version < 9 {
+			brokerMetadata.Rack, idx = readRackNullableString(bytes, idx)
+		} else if version == 9 {
+			brokerMetadata.Rack, idx = readRack(bytes, idx)
+		}
 		if version == 9 {
 			idx = readTaggedField(bytes, idx)
 		}
 		metadataResp.BrokerMetadataList[i] = brokerMetadata
 	}
-	if version == 9 {
+	if version > 1 && version < 9 {
+		metadataResp.ClusterId, idx = readClusterIdNullableString(bytes, idx)
+	} else if version == 9 {
 		metadataResp.ClusterId, idx = readClusterId(bytes, idx)
 	}
-	metadataResp.ControllerId, idx = readControllerId(bytes, idx)
+	if version > 0 {
+		metadataResp.ControllerId, idx = readControllerId(bytes, idx)
+	}
 	var topicLength int
-	if version == 1 {
+	if version < 9 {
 		topicLength, idx = readArrayLen(bytes, idx)
 	} else if version == 9 {
 		topicLength, idx = readCompactArrayLen(bytes, idx)
@@ -109,14 +119,16 @@ func DecodeMetadataResp(bytes []byte, version int16) (metadataResp *MetadataResp
 	for i := 0; i < topicLength; i++ {
 		topicMetadata := &TopicMetadata{}
 		topicMetadata.ErrorCode, idx = readErrorCode(bytes, idx)
-		if version == 1 {
+		if version < 9 {
 			topicMetadata.Topic, idx = readString(bytes, idx)
 		} else if version == 9 {
 			topicMetadata.Topic, idx = readCompactString(bytes, idx)
 		}
-		topicMetadata.IsInternal, idx = readBool(bytes, idx)
+		if version > 0 {
+			topicMetadata.IsInternal, idx = readBool(bytes, idx)
+		}
 		var partitionLength int
-		if version == 1 {
+		if version < 9 {
 			partitionLength, idx = readArrayLen(bytes, idx)
 		} else if version == 9 {
 			partitionLength, idx = readCompactArrayLen(bytes, idx)
@@ -126,10 +138,12 @@ func DecodeMetadataResp(bytes []byte, version int16) (metadataResp *MetadataResp
 			partitionMetadata := &PartitionMetadata{}
 			partitionMetadata.ErrorCode, idx = readErrorCode(bytes, idx)
 			partitionMetadata.PartitionId, idx = readPartitionId(bytes, idx)
-			partitionMetadata.LeaderId, idx = readInt32(bytes, idx)
-			partitionMetadata.LeaderEpoch, idx = readInt32(bytes, idx)
+			partitionMetadata.LeaderId, idx = readLeaderId(bytes, idx)
+			if version > 6 && version <= 9 {
+				partitionMetadata.LeaderEpoch, idx = readLeaderEpoch(bytes, idx)
+			}
 			var replicaLength int
-			if version == 1 {
+			if version < 9 {
 				replicaLength, idx = readArrayLen(bytes, idx)
 			} else if version == 9 {
 				replicaLength, idx = readCompactArrayLen(bytes, idx)
@@ -141,7 +155,7 @@ func DecodeMetadataResp(bytes []byte, version int16) (metadataResp *MetadataResp
 				partitionMetadata.Replicas[k] = replica
 			}
 			var caughtReplicaLength int
-			if version == 1 {
+			if version < 9 {
 				caughtReplicaLength, idx = readArrayLen(bytes, idx)
 			} else if version == 9 {
 				caughtReplicaLength, idx = readCompactArrayLen(bytes, idx)
@@ -152,30 +166,36 @@ func DecodeMetadataResp(bytes []byte, version int16) (metadataResp *MetadataResp
 				replica.ReplicaId, idx = readReplicaId(bytes, idx)
 				partitionMetadata.CaughtReplicas[k] = replica
 			}
-			var offlineReplicaLength int
-			if version == 1 {
-				offlineReplicaLength, idx = readArrayLen(bytes, idx)
-			} else if version == 9 {
-				offlineReplicaLength, idx = readCompactArrayLen(bytes, idx)
-			}
-			partitionMetadata.OfflineReplicas = make([]*Replica, offlineReplicaLength)
-			for k := 0; k < offlineReplicaLength; k++ {
-				replica := &Replica{}
-				replica.ReplicaId, idx = readReplicaId(bytes, idx)
-				partitionMetadata.OfflineReplicas[k] = replica
+			if version > 4 && version <= 9 {
+				var offlineReplicaLength int
+				if version < 9 {
+					offlineReplicaLength, idx = readArrayLen(bytes, idx)
+				} else if version == 9 {
+					offlineReplicaLength, idx = readCompactArrayLen(bytes, idx)
+				}
+				partitionMetadata.OfflineReplicas = make([]*Replica, offlineReplicaLength)
+				for k := 0; k < offlineReplicaLength; k++ {
+					replica := &Replica{}
+					replica.ReplicaId, idx = readReplicaId(bytes, idx)
+					partitionMetadata.OfflineReplicas[k] = replica
+				}
 			}
 			topicMetadata.PartitionMetadataList[j] = partitionMetadata
 			if version == 9 {
 				idx = readTaggedField(bytes, idx)
 			}
 		}
-		topicMetadata.TopicAuthorizedOperation, idx = readTopicAuthorizedOperation(bytes, idx)
+		if version > 7 && version <= 9 {
+			topicMetadata.TopicAuthorizedOperation, idx = readTopicAuthorizedOperation(bytes, idx)
+		}
 		metadataResp.TopicMetadataList[i] = topicMetadata
 		if version == 9 {
 			idx = readTaggedField(bytes, idx)
 		}
 	}
-	metadataResp.ClusterAuthorizedOperation, idx = readClusterAuthorizedOperation(bytes, idx)
+	if version > 7 && version <= 9 {
+		metadataResp.ClusterAuthorizedOperation, idx = readClusterAuthorizedOperation(bytes, idx)
+	}
 	if version == 9 {
 		idx = readTaggedField(bytes, idx)
 	}
@@ -184,27 +204,27 @@ func DecodeMetadataResp(bytes []byte, version int16) (metadataResp *MetadataResp
 
 func (m *MetadataResp) BytesLength(version int16) int {
 	result := LenCorrId
-	if version == 8 || version == 9 {
-		result += LenThrottleTime
-	}
 	if version == 9 {
 		result += LenTaggedField
 	}
-	if version == 1 || version == 8 {
+	if version > 2 && version <= 9 {
+		result += LenThrottleTime
+	}
+	if version < 9 {
 		result += LenArray
 	} else if version == 9 {
 		result += CompactArrayLen(len(m.BrokerMetadataList))
 	}
 	for _, val := range m.BrokerMetadataList {
 		result += LenNodeId
-		if version == 1 || version == 8 {
+		if version < 9 {
 			result += StrLen(val.Host)
 		} else if version == 9 {
 			result += CompactStrLen(val.Host)
 		}
 		result += LenPort
-		if version == 1 || version == 8 {
-			result += 2
+		if version > 0 && version < 9 {
+			result += NullableStrLen(val.Rack)
 		} else if version == 9 {
 			result += CompactNullableStrLen(val.Rack)
 		}
@@ -212,40 +232,40 @@ func (m *MetadataResp) BytesLength(version int16) int {
 			result += LenTaggedField
 		}
 	}
-	if version == 8 {
-		result += StrLen(m.ClusterId)
+	if version > 1 && version < 9 {
+		result += NullableStrLen(&m.ClusterId)
 	} else if version == 9 {
 		result += CompactStrLen(m.ClusterId)
 	}
-	result += LenControllerId
-	if version == 1 || version == 8 {
+	if version > 0 {
+		result += LenControllerId
+	}
+	if version < 9 {
 		result += LenArray
 	} else if version == 9 {
 		result += CompactArrayLen(len(m.TopicMetadataList))
 	}
 	for _, val := range m.TopicMetadataList {
 		result += LenErrorCode
-		if version == 1 || version == 8 {
+		if version < 9 {
 			result += StrLen(val.Topic)
 		} else if version == 9 {
 			result += CompactStrLen(val.Topic)
 		}
-		if version == 1 || version == 8 {
+		if version > 0 {
 			result += LenIsInternalV1
-		} else if version == 9 {
-			result += LenIsInternalV9
 		}
-		if version == 1 || version == 8 {
+		if version < 9 {
 			result += LenArray
 		} else if version == 9 {
 			result += CompactArrayLen(len(val.PartitionMetadataList))
 		}
 		for _, partition := range val.PartitionMetadataList {
 			result += LenErrorCode + LenPartitionId + LenLeaderId
-			if version == 8 || version == 9 {
+			if version > 6 && version <= 9 {
 				result += LenLeaderEpoch
 			}
-			if version == 1 || version == 8 {
+			if version < 9 {
 				result += LenArray
 			} else if version == 9 {
 				result += CompactArrayLen(len(partition.Replicas))
@@ -253,7 +273,7 @@ func (m *MetadataResp) BytesLength(version int16) int {
 			for range partition.Replicas {
 				result += LenReplicaId
 			}
-			if version == 1 || version == 8 {
+			if version < 9 {
 				result += LenArray
 			} else if version == 9 {
 				result += CompactArrayLen(len(partition.CaughtReplicas))
@@ -261,27 +281,28 @@ func (m *MetadataResp) BytesLength(version int16) int {
 			for range partition.CaughtReplicas {
 				result += LenReplicaId
 			}
-			if version == 8 {
-				result += LenArray
+			if version > 4 && version <= 9 {
+				if version < 9 {
+					result += LenArray
+				} else if version == 9 {
+					result += CompactArrayLen(len(partition.OfflineReplicas))
+				}
 				for range partition.OfflineReplicas {
 					result += LenReplicaId
 				}
-			} else if version == 9 {
-				result += CompactArrayLen(len(partition.OfflineReplicas))
-				for range partition.OfflineReplicas {
-					result += LenReplicaId
-				}
+			}
+			if version == 9 {
 				result += LenTaggedField
 			}
 		}
-		if version == 8 || version == 9 {
+		if version > 7 && version <= 9 {
 			result += LenTopicAuthOperation
 		}
 		if version == 9 {
 			result += LenTaggedField
 		}
 	}
-	if version == 8 || version == 9 {
+	if version > 7 && version <= 9 {
 		result += LenClusterAuthOperation
 	}
 	if version == 9 {
@@ -294,31 +315,27 @@ func (m *MetadataResp) Bytes(version int16) []byte {
 	bytes := make([]byte, m.BytesLength(version))
 	idx := 0
 	idx = putCorrId(bytes, idx, m.CorrelationId)
-	if version == 8 || version == 9 {
-		idx = putThrottleTime(bytes, idx, 0)
-	}
 	if version == 9 {
 		idx = putTaggedField(bytes, idx)
 	}
-	if version == 1 || version == 8 {
-		idx = putInt(bytes, idx, len(m.BrokerMetadataList))
+	if version > 2 && version <= 9 {
+		idx = putThrottleTime(bytes, idx, 0)
+	}
+	if version < 9 {
+		idx = putArrayLen(bytes, idx, len(m.BrokerMetadataList))
 	} else if version == 9 {
-		idx = putVarint(bytes, idx, len(m.BrokerMetadataList))
+		idx = putCompactArrayLen(bytes, idx, len(m.BrokerMetadataList))
 	}
 	for _, brokerMetadata := range m.BrokerMetadataList {
 		idx = putNodeId(bytes, idx, brokerMetadata.NodeId)
-		if version == 1 || version == 8 {
+		if version < 9 {
 			idx = putHostString(bytes, idx, brokerMetadata.Host)
 		} else if version == 9 {
 			idx = putHost(bytes, idx, brokerMetadata.Host)
 		}
 		idx = putBrokerPort(bytes, idx, brokerMetadata.Port)
-		if version == 1 || version == 8 {
-			// todo
-			bytes[idx] = 255
-			idx += 1
-			bytes[idx] = 255
-			idx += 1
+		if version > 0 && version < 9 {
+			idx = putRackNullableString(bytes, idx, brokerMetadata.Rack)
 		} else if version == 9 {
 			idx = putRack(bytes, idx, brokerMetadata.Rack)
 		}
@@ -326,27 +343,31 @@ func (m *MetadataResp) Bytes(version int16) []byte {
 			idx = putTaggedField(bytes, idx)
 		}
 	}
-	if version == 8 {
-		idx = putClusterIdString(bytes, idx, m.ClusterId)
+	if version > 1 && version < 9 {
+		idx = putClusterIdNullableString(bytes, idx, m.ClusterId)
 	} else if version == 9 {
 		idx = putClusterId(bytes, idx, m.ClusterId)
 	}
-	idx = putControllerId(bytes, idx, m.ControllerId)
-	if version == 1 || version == 8 {
-		idx = putInt(bytes, idx, len(m.TopicMetadataList))
+	if version > 0 {
+		idx = putControllerId(bytes, idx, m.ControllerId)
+	}
+	if version < 9 {
+		idx = putArrayLen(bytes, idx, len(m.TopicMetadataList))
 	} else if version == 9 {
-		idx = putVarint(bytes, idx, len(m.TopicMetadataList))
+		idx = putCompactArrayLen(bytes, idx, len(m.TopicMetadataList))
 	}
 	for _, topicMetadata := range m.TopicMetadataList {
 		idx = putErrorCode(bytes, idx, topicMetadata.ErrorCode)
-		if version == 1 || version == 8 {
+		if version < 9 {
 			idx = putTopicString(bytes, idx, topicMetadata.Topic)
 		} else if version == 9 {
 			idx = putTopic(bytes, idx, topicMetadata.Topic)
 		}
-		idx = putBool(bytes, idx, topicMetadata.IsInternal)
-		if version == 1 || version == 8 {
-			idx = putInt(bytes, idx, len(topicMetadata.PartitionMetadataList))
+		if version > 0 {
+			idx = putBool(bytes, idx, topicMetadata.IsInternal)
+		}
+		if version < 9 {
+			idx = putArrayLen(bytes, idx, len(topicMetadata.PartitionMetadataList))
 		} else if version == 9 {
 			idx = putCompactArrayLen(bytes, idx, len(topicMetadata.PartitionMetadataList))
 		}
@@ -354,47 +375,47 @@ func (m *MetadataResp) Bytes(version int16) []byte {
 			idx = putErrorCode(bytes, idx, partitionMetadata.ErrorCode)
 			idx = putPartitionId(bytes, idx, partitionMetadata.PartitionId)
 			idx = putLeaderId(bytes, idx, partitionMetadata.LeaderId)
-			if version == 8 || version == 9 {
+			if version > 6 && version <= 9 {
 				idx = putLeaderEpoch(bytes, idx, partitionMetadata.LeaderEpoch)
 			}
-			if version == 1 || version == 8 {
-				idx = putInt(bytes, idx, len(partitionMetadata.Replicas))
+			if version < 9 {
+				idx = putArrayLen(bytes, idx, len(partitionMetadata.Replicas))
 			} else if version == 9 {
-				idx = putVarint(bytes, idx, len(partitionMetadata.Replicas))
+				idx = putCompactArrayLen(bytes, idx, len(partitionMetadata.Replicas))
 			}
 			for _, replica := range partitionMetadata.Replicas {
 				idx = putReplicaId(bytes, idx, replica.ReplicaId)
 			}
-			if version == 1 || version == 8 {
-				idx = putInt(bytes, idx, len(partitionMetadata.CaughtReplicas))
+			if version < 9 {
+				idx = putArrayLen(bytes, idx, len(partitionMetadata.CaughtReplicas))
 			} else if version == 9 {
-				idx = putVarint(bytes, idx, len(partitionMetadata.CaughtReplicas))
+				idx = putCompactArrayLen(bytes, idx, len(partitionMetadata.CaughtReplicas))
 			}
 			for _, replica := range partitionMetadata.CaughtReplicas {
 				idx = putReplicaId(bytes, idx, replica.ReplicaId)
 			}
-			if version == 8 {
-				idx = putInt(bytes, idx, len(partitionMetadata.OfflineReplicas))
+			if version > 4 && version <= 9 {
+				if version < 9 {
+					idx = putArrayLen(bytes, idx, len(partitionMetadata.OfflineReplicas))
+				} else if version == 9 {
+					idx = putCompactArrayLen(bytes, idx, len(partitionMetadata.OfflineReplicas))
+				}
 				for _, replica := range partitionMetadata.OfflineReplicas {
 					idx = putReplicaId(bytes, idx, replica.ReplicaId)
 				}
-			} else if version == 9 {
-				bytes[idx] = byte(len(partitionMetadata.OfflineReplicas) + 1)
-				idx++
-				for _, replica := range partitionMetadata.OfflineReplicas {
-					idx = putReplicaId(bytes, idx, replica.ReplicaId)
-				}
+			}
+			if version == 9 {
 				idx = putTaggedField(bytes, idx)
 			}
 		}
-		if version == 8 || version == 9 {
+		if version > 7 && version <= 9 {
 			idx = putTopicAuthorizedOperation(bytes, idx, topicMetadata.TopicAuthorizedOperation)
 		}
 		if version == 9 {
 			idx = putTaggedField(bytes, idx)
 		}
 	}
-	if version == 8 || version == 9 {
+	if version > 7 && version <= 9 {
 		idx = putClusterAuthorizedOperation(bytes, idx, m.ClusterAuthorizedOperation)
 	}
 	if version == 9 {
