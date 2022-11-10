a780601b8d9b5de37dc43176edc53b77573aaeaa
ZhangJian He <shoothzj@gmail.com>
2022-11-10 21:11:21 +0800

    optimize util_general_string_test (#56)

--- codec/util_general_string_test.go
+++ codec/util_general_string_test.go
@@ -19,6 +19,7 @@ package codec
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -28,7 +29,7 @@ func TestCompactStr(t *testing.T) {
 	value2 := string(testHex2Bytes(t, "88"))
 	putCompactString(bytes2, 0, value2)
 	res2, idx2 := readCompactString(bytes2, 0)
-	assert.Equal(t, idx2, 2)
+	assert.Equal(t, 2, idx2)
 	assert.Equal(t, value2, res2)
 
 	// 127bytes
@@ -36,24 +37,24 @@ func TestCompactStr(t *testing.T) {
 	value3 := string(testHex2Bytes(t, "00000006006d5f5f5f546573744b61666b61436f6e73756d655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29ffffffff0000232600000001"))
 	putCompactString(bytes3, 0, value3)
 	res3, idx3 := readCompactString(bytes3, 0)
-	assert.Equal(t, idx3, 129)
-	assert.Equal(t, res3, value3)
+	assert.Equal(t, 129, idx3)
+	assert.Equal(t, value3, res3)
 
 	// 128 bytes
 	bytes4 := make([]byte, 130)
 	value4 := string(testHex2Bytes(t, "00000006006d5f5f5f546573744b61666b61436f6e73756d655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29ffffffff000023260000000100"))
 	putCompactString(bytes4, 0, value4)
 	res4, idx4 := readCompactString(bytes4, 0)
-	assert.Equal(t, idx4, 130)
-	assert.Equal(t, res4, value4)
+	assert.Equal(t, 130, idx4)
+	assert.Equal(t, value4, res4)
 
 	// 256 bytes
 	bytes5 := make([]byte, 258)
 	value5 := string(testHex2Bytes(t, "00000006006d5f5f5f546573744b61666b61436f6e73756d655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29ffffffff00002326000000010000000006006d5f5f5f546573744b61666b61436f6e73756d655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29ffffffff000023260000000100"))
 	putCompactString(bytes5, 0, value5)
 	res5, idx5 := readCompactString(bytes5, 0)
-	assert.Equal(t, idx5, 258)
-	assert.Equal(t, res5, value5)
+	assert.Equal(t, 258, idx5)
+	assert.Equal(t, value5, res5)
 }
 
 func TestCompactNullableStr(t *testing.T) {
@@ -61,7 +62,7 @@ func TestCompactNullableStr(t *testing.T) {
 	bytes1 := make([]byte, 1)
 	putCompactStringNullable(bytes1, 0, nil)
 	res1, idx1 := readCompactStringNullable(bytes1, 0)
-	assert.Equal(t, idx1, 1)
+	assert.Equal(t, 1, idx1)
 	assert.Nil(t, res1)
 
 	// 1 bytes
@@ -69,7 +70,7 @@ func TestCompactNullableStr(t *testing.T) {
 	value2 := string(testHex2Bytes(t, "88"))
 	putCompactStringNullable(bytes2, 0, &value2)
 	res2, idx2 := readCompactStringNullable(bytes2, 0)
-	assert.Equal(t, idx2, 2)
+	assert.Equal(t, 2, idx2)
 	assert.Equal(t, value2, *res2)
 
 	// 127bytes
@@ -77,22 +78,30 @@ func TestCompactNullableStr(t *testing.T) {
 	value3 := string(testHex2Bytes(t, "00000006006d5f5f5f546573744b61666b61436f6e73756d655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29ffffffff0000232600000001"))
 	putCompactStringNullable(bytes3, 0, &value3)
 	res3, idx3 := readCompactStringNullable(bytes3, 0)
-	assert.Equal(t, idx3, 129)
-	assert.Equal(t, *res3, value3)
+	assert.Equal(t, 129, idx3)
+	assert.Equal(t, value3, *res3)
 
 	// 128 bytes
 	bytes4 := make([]byte, 130)
 	value4 := string(testHex2Bytes(t, "00000006006d5f5f5f546573744b61666b61436f6e73756d655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29ffffffff000023260000000100"))
 	putCompactStringNullable(bytes4, 0, &value4)
 	res4, idx4 := readCompactStringNullable(bytes4, 0)
-	assert.Equal(t, idx4, 130)
-	assert.Equal(t, *res4, value4)
+	assert.Equal(t, 130, idx4)
+	assert.Equal(t, value4, *res4)
 
 	// 256 bytes
 	bytes5 := make([]byte, 258)
 	value5 := string(testHex2Bytes(t, "00000006006d5f5f5f546573744b61666b61436f6e73756d655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29ffffffff00002326000000010000000006006d5f5f5f546573744b61666b61436f6e73756d655f696e5f676f5f64656d6f5f64656d6f5f6b61666b612e746573744068657a68616e676a69616e64654d6163426f6f6b2d50726f2e6c6f63616c20286769746875622e636f6d2f7365676d656e74696f2f6b61666b612d676f29ffffffff000023260000000100"))
 	putCompactStringNullable(bytes5, 0, &value5)
 	res5, idx5 := readCompactStringNullable(bytes5, 0)
-	assert.Equal(t, idx5, 258)
-	assert.Equal(t, *res5, value5)
+	assert.Equal(t, 258, idx5)
+	assert.Equal(t, value5, *res5)
+}
+
+func TestCompactStrLen(t *testing.T) {
+	{
+		value := strings.Repeat("a", 1)
+		strLen := CompactStrLen(value)
+		assert.Equal(t, 2, strLen)
+	}
 }
