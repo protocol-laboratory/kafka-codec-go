edf18ed2b75f690ab55e7b992f5ef1b63eae165d
ZhiXin Jin <40939066+tyluffy@users.noreply.github.com>
2022-11-28 13:52:38 +0800

    fix consume failed when record value length is between 64bytes and 128bytes (#59)

--- codec/record_batch_test.go
+++ codec/record_batch_test.go
@@ -19,6 +19,7 @@ package codec
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -45,4 +46,54 @@ func TestEncodeRecordBatch(t *testing.T) {
 	r.Records[0] = record
 	r.Bytes()
 	assert.Equal(t, 76, r.BytesLength())
+}
+
+func TestEncodeRecordBatch64Bytes(t *testing.T) {
+	r := &RecordBatch{}
+	r.Offset = 0
+	r.MessageSize = 64
+	r.LeaderEpoch = 0
+	r.MagicByte = 2
+	r.Flags = 0
+	r.LastOffsetDelta = 0
+	r.FirstTimestamp = 1625962021853
+	r.LastTimestamp = 1625962021853
+	r.ProducerId = -1
+	r.ProducerEpoch = -1
+	r.BaseSequence = -1
+	r.Records = make([]*Record, 1)
+	record := &Record{}
+	record.RecordAttributes = 0
+	record.RelativeTimestamp = 0
+	record.RelativeOffset = 0
+	record.Key = nil
+	record.Value = []byte(strings.Repeat("a", 64))
+	r.Records[0] = record
+	r.Bytes()
+	assert.Equal(t, 134, r.BytesLength())
+}
+
+func TestEncodeRecordBatch128Bytes(t *testing.T) {
+	r := &RecordBatch{}
+	r.Offset = 0
+	r.MessageSize = 64
+	r.LeaderEpoch = 0
+	r.MagicByte = 2
+	r.Flags = 0
+	r.LastOffsetDelta = 0
+	r.FirstTimestamp = 1625962021853
+	r.LastTimestamp = 1625962021853
+	r.ProducerId = -1
+	r.ProducerEpoch = -1
+	r.BaseSequence = -1
+	r.Records = make([]*Record, 1)
+	record := &Record{}
+	record.RecordAttributes = 0
+	record.RelativeTimestamp = 0
+	record.RelativeOffset = 0
+	record.Key = nil
+	record.Value = []byte(strings.Repeat("a", 128))
+	r.Records[0] = record
+	r.Bytes()
+	assert.Equal(t, 198, r.BytesLength())
 }
